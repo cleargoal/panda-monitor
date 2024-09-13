7c3e0352f244c85d7050c6ccee2119bb7862abda
yefrem <cleargoal01@gmail.com>
2024-09-13 19:00:12 +0300

    create api.php routes file

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types = 1);
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::middleware(['auth:sanctum',])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/auth-user', [AuthController::class, 'authUser']);
+});
