c42dabfea73285cc7154c60dd6716bf32b5de26d
yefrem <cleargoal01@gmail.com>
2024-09-14 18:23:16 +0300

    add route Route::apiResource('/source',

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,16 @@
 <?php
+
 declare(strict_types = 1);
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SourceController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::middleware(['auth:sanctum',])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/auth-user', [AuthController::class, 'authUser']);
+
+    Route::apiResource('/source', SourceController::class);
 });
