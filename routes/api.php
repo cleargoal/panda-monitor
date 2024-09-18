a5f0909bb7f5320964ad36361b4e30fb7801768b
yefrem <cleargoal01@gmail.com>
2024-09-18 16:10:17 +0300

    fix routes and add middleware

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 use App\Http\Controllers\AdvertController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PriceController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login'])->name('login');
@@ -13,6 +12,5 @@ Route::middleware(['auth:sanctum',])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/auth-user', [AuthController::class, 'authUser']);
 
-    Route::apiResource('/prices', PriceController::class);
-    Route::apiResource('/adverts', AdvertController::class);
+    Route::apiResource('/adverts', AdvertController::class)->middleware('own');
 });
