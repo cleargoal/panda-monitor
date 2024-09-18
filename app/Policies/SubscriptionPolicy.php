787f3849b5644a47422edd0db47186576bd109e1
yefrem <cleargoal01@gmail.com>
2024-09-18 11:10:54 +0300

    fix SubscriptionPolicy.php

--- app/Policies/SubscriptionPolicy.php
+++ app/Policies/SubscriptionPolicy.php
@@ -4,14 +4,14 @@ declare(strict_types = 1);
 
 namespace App\Policies;
 
-use App\Models\Price;
+use App\Models\Advert;
 use App\Models\User;
 
 class SubscriptionPolicy
 {
-    public function own(User $user, Price $price): bool
+    public function own(User $user, Advert $advert): bool
     {
-        return $user->prices()->where('project_id', $price->id)->exists();
+        return $user->adverts()->where('advert_id', $advert->id)->exists();
     }
 
 }
