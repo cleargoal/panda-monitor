e838bc3a5b5766e8732d9a971b16301290826903
yefrem <cleargoal01@gmail.com>
2024-09-20 22:13:58 +0300

    change 'console.php' to make scheduler

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+declare(strict_types=1);
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+use App\Services\MonitorService;
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::call(new MonitorService())->dailyAt('2:00');
