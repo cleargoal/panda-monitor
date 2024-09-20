7323102c2bc677b3222a8dd95a1d5ae7df985f9a
yefrem <cleargoal01@gmail.com>
2024-09-20 20:33:31 +0300

    change 'email' field param

--- database/migrations/2024_09_17_110757_create_adverts_table.php
+++ database/migrations/2024_09_17_110757_create_adverts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('url');
             $table->string('name')->comment('Products name');
-            $table->string('email')->comment('email for price notifications');
+            $table->string('email')->nullable()->comment('email for price notifications');
             $table->integer('price');
             $table->timestamps();
         });
