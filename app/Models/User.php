2ff163eddbf20ccf4d8632afcfed0b4ca773b00e
yefrem <cleargoal01@gmail.com>
2024-09-22 18:24:26 +0300

    adjust relation 'adverts()'

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,6 +51,6 @@ class User extends Authenticatable
 
     public function adverts(): BelongsToMany
     {
-        return $this->belongsToMany(Advert::class);
+        return $this->belongsToMany(Advert::class)->withPivot('email');
     }
 }
