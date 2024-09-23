8c1b7b5612a0fddad6aa9dc73163795e17e5d581
yefrem <cleargoal01@gmail.com>
2024-09-23 18:57:49 +0300

    create Price

---
+++ app/Notifications/PriceChangedNotification.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class PriceChangedNotification extends Notification implements ShouldQueue
+{
+    use Queueable, SerializesModels, InteractsWithQueue;
+
+    private array $data;
+
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $mailMessage = (new MailMessage)
+            ->subject('Price Changed')
+            ->greeting('Advert price is changed.');
+
+        foreach ($this->data as $urlData) {
+            $mailMessage->line( 'Price: ' . $urlData['price'] . ' of URL: ' . $urlData['sourceUrl']);
+        }
+
+        $mailMessage->line('Thank you for using our application!');
+
+        return $mailMessage;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
