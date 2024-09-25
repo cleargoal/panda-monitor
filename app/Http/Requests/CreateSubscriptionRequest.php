6bc04ac07186aeb9cb1bd74a7c953d32c5b4d5ba
yefrem <cleargoal01@gmail.com>
2024-09-25 19:55:18 +0300

    create CreateSubscriptionRequest.php

---
+++ app/Http/Requests/CreateSubscriptionRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateSubscriptionRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'url' => ['required', 'string', 'url'],
+            'email' => ['nullable', 'string', 'email'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'url.required' => 'The URL is required.',
+            'url.url' => 'The URL must be a valid format.',
+            'email.email' => 'The email must be a valid email address.',
+        ];
+    }
+}
