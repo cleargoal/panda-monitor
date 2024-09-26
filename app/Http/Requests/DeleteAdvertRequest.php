e4207052b7c8db8b7996e87e9a752d47c1267f4e
yefrem <cleargoal01@gmail.com>
2024-09-26 13:51:54 +0300

    create DeleteAdvertRequest.php

---
+++ app/Http/Requests/DeleteAdvertRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DeleteAdvertRequest extends FormRequest
+{
+    public function authorize(): true
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'advert' => 'required|integer|exists:adverts,id',
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'advert' => 'advert ID',
+        ];
+    }
+}
