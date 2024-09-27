a8129fa33936dc5696904f08e6086cacd61dd142
yefrem <cleargoal01@gmail.com>
2024-09-27 20:41:11 +0300

    fix ParserService.php

--- app/Services/ParserService.php
+++ app/Services/ParserService.php
@@ -4,19 +4,17 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class ParserService
 {
 
-    public function getDataFromPage(string $sourceUrl): array
+    public function getDataFromPage(string $sourceUrl): array|null
     {
         $content = file_get_contents($sourceUrl);
         $jsonString = Str::between($content, '<script data-rh="true" type="application/ld+json">', '</script><script defer="defer"');
         $advertData = json_decode($jsonString, true);
-        return ['price' => $advertData['offers']['price'], 'name' => $advertData['name']];
+        return $advertData ? ['price' => $advertData['offers']['price'], 'name' => $advertData['name']] : ['price' => null, 'name' => null];
     }
 
 }
