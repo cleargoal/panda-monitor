011a978c734f649cb287bd81095ac65c60a20c4c
yefrem <cleargoal01@gmail.com>
2024-10-01 21:40:37 +0300

    change 'verify-link' route and add '/verification-notification'

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,14 @@
 declare(strict_types = 1);
 
 use App\Http\Controllers\AdvertController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/verify/{id}/{hash}', [AuthController::class, 'verifyEmailByLink'])->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
-Route::middleware(['auth:sanctum',])->group(function () {
+Route::post('/verify-link', [AuthController::class, 'verifyEmailByLink'])->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/auth-user', [AuthController::class, 'authUser']);
 
@@ -17,3 +18,8 @@ Route::middleware(['auth:sanctum',])->group(function () {
     Route::post('/adverts', [AdvertController::class, 'store']);
     Route::delete('/adverts/{advert}', [AdvertController::class, 'destroy'])->name('adverts.destroy')->can('destroy','advert');
 });
+Route::post('/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return response()->json(['message' => 'Verification link sent!']);
+})->middleware(['auth:sanctum', 'throttle:6,1'])->name('verification.send');
